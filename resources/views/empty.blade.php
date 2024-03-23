ca7a08b5fdb770e2138fefda2c035d9ee8f65914
devnacer <kalache.nacer.kn@gmail.com>
2024-03-23 07:12:14 +0100

    first commit

---
+++ resources/views/empty.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+
+@section('title')
+    home page
+@endsection
+
+@section('css')
+@endsection
+
+@section('breadcrumb_1')
+    Home
+@endsection
+
+@section('breadcrumb_2')
+    Section
+@endsection
+
+@section('content')
+    
+@endsection
+
+@section('scripts')
+    
+@endsection
