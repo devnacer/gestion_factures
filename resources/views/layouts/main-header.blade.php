8c96278fc55f845a7395f21995c6ad4c3e69f34a
devnacer <kalache.nacer.kn@gmail.com>
2024-03-24 15:42:40 +0100

    edit flags

--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -171,10 +171,29 @@
         </li>
         <!-- Language Dropdown Menu -->
         <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
+            {{-- <a class="nav-link" data-toggle="dropdown" href="#">
                 <i class="flag-icon flag-icon-us"></i>
+            </a> --}}
+            <a href="#" class="nav-link d-flex nav-item nav-link pl-0 country-flag1" data-toggle="dropdown" aria-expanded="false">
+                @if (App::getLocale() == 'ar')
+                    <span class="avatar country-Flag mr-0 align-self-center bg-transparent"><img
+                            src="{{ URL::asset('assets/img/flags/algeria_flag.png') }}"
+                            alt="img"></span>
+                    <strong class="mr-2 ml-2 my-auto">{{ LaravelLocalization::getCurrentLocaleName() }}</strong>
+                @elseif (App::getLocale() == 'fr')
+                    <span class="avatar country-Flag mr-0 align-self-center bg-transparent"><img
+                            src="{{ URL::asset('assets/img/flags/french_flag.jpg') }}"
+                            alt="img"></span>
+                    <strong class="mr-2 ml-2 my-auto">{{ LaravelLocalization::getCurrentLocaleName() }}</strong>
+                @else
+                    <span class="avatar country-Flag mr-0 align-self-center bg-transparent"><img
+                            src="{{ URL::asset('assets/img/flags/us_flag.jpg') }}"
+                            alt="img"></span>
+                    <strong class="mr-2 ml-2 my-auto">{{ LaravelLocalization::getCurrentLocaleName() }}</strong>
+                @endif
+                <div class="my-auto"></div>
             </a>
-            <div class="dropdown-menu dropdown-menu-right p-0">
+            {{-- <div class="dropdown-menu dropdown-menu-right p-0">
                 <a href="#" class="dropdown-item active">
                     <i class="flag-icon flag-icon-us mr-2"></i> English
                 </a>
@@ -187,7 +206,24 @@
                 <a href="#" class="dropdown-item">
                     <i class="flag-icon flag-icon-es mr-2"></i> Spanish
                 </a>
+            </div> --}}
+
+            <div class="dropdown-menu dropdown-menu-right p-0" x-placement="bottom-end">
+                @foreach (LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                    <a class="dropdown-item" rel="alternate" hreflang="{{ $localeCode }}"
+                        href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+                        @if ($properties['native'] == 'English')
+                            <i class="flag-icon flag-icon-us mr-2"></i>
+                        @elseif($properties['native'] == 'العربية')
+                            <i class="flag-icon flag-icon-dz mr-2"></i>
+                        @elseif($properties['native'] == 'français')
+                            <i class="flag-icon flag-icon-fr mr-2"></i>
+                        @endif
+                        {{ $properties['native'] }}
+                    </a>
+                @endforeach
             </div>
+
         </li>
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
